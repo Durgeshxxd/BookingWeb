eb60c8515bc2d9892092e545b6983ab59dce3dd5
Durgesh Upadhyaya <durgeshupadhyay660@gmail.com>
2024-11-17 20:13:05 +0530

    configchan

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  root: './', // This points to your project root
-  build: {
-    outDir: 'dist', // Output directory for the build
-  },
-});
+  plugins: [react()],
+})
