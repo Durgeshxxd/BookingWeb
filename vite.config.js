24c433bc510de1a0462e0c8e9267cba0f0c42321
Durgesh Upadhyaya <durgeshupadhyay660@gmail.com>
2024-11-17 20:05:47 +0530

    first

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: './', // This points to your project root
+  build: {
+    outDir: 'dist', // Output directory for the build
+  },
+});
